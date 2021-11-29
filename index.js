ddd80aa983501de068bd59cd1d2cc7a9ebf6fea9
Sabrina-Ibrahim <Sabrina.ibrahim24@me.com>
2021-11-28 21:30:58 -0600

    add restaurant random preference

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ let places = ["Greece", "Cuba", "Japan", "Thailand"];
 let random = places[Math.floor(Math.random() * places.length)]
 console.log(random);
 
+//As a user, I want a restaurant to be randomly selected for my day trip.
+let restaurant = ["The Spring Oyster", "Skyward", "The Signature Room", "Single Snhot"]
+let randomFood = restaurant[Math.floor(Math.random ()* restaurant.length)]
+console.log(randomFood);
