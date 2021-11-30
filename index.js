cb8b6d035c7fd937f89c0a9554651e0397461606
Sabrina-Ibrahim <Sabrina.ibrahim24@me.com>
2021-11-29 18:46:08 -0600

    fix grammatical errors, remove multiple console.logs, and combine them into one output.

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+"use strict";
 //As a user, I want a destination to be randomly selected for my day trip.
 //pick multiple places and create a generator to output more than one place.
 //try an array and loop.
-"use strict"
 
 let places = ["Greece", "Cuba", "Japan", "Thailand"];
 let random = places[Math.floor(Math.random() * places.length)]
-console.log(random);
 
 //As a user, I want a restaurant to be randomly selected for my day trip.
-let restaurant = ["The Spring Oyster", "Skyward", "The Signature Room", "Single Snhot"]
-let randomFood = restaurant[Math.floor(Math.random ()* restaurant.length)]
-console.log(randomFood);
+let restaurant = ["The Spring Oyster", "Skyward", "The Signature Room", "Single Shot"]
+let randomFood = restaurant[Math.floor(Math.random() * restaurant.length)]
+
+//As a user, I want a mode of transportation to be randomly selected for my day trip.
+let transportation = ["Private Jet", "Boat", "Tesla", "Train"]
+let rantrans = transportation [Math.floor(Math.random() * transportation.length)]
+
+//As a user, I want a form of entertainment to be randomly selected for my day trip.
+let entertainment = ["Comedy Show", "Hiking", "Karaoke", "Amusement Parks"]
+let funThings = entertainment[Math.floor(Math.random() * entertainment.length)];
+console.log(random + (", ") + randomFood + (", ") + rantrans + (", and ") + funThings);
